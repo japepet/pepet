b5e9b383c3f9c2d54d5b3ab4d98d1e595e411c29
japepet <jppogi849@gmail.com>
2024-10-11 23:24:04 +0800

    Add files via upload

---
+++ CREATE DATABASE my_database;.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE my_database; 
+USE my_database;
+CREATE TABLE users ( id VARCHAR(7), name(VARCHAR(50) );
+INSERT INTO users(id,name) VALUES ('23-0679', 'Jester Paul Florendo');
+SELECT * FROM users;
